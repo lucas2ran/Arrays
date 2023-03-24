7abfbdd2531995aaaa8c86223d67cc6a8a10f3ef
lucas vinicius <lucas.thuran@hotmail.com>
2023-03-23 22:59:03 -0300

    QueuePilha

--- ExQueue.java
+++ ExQueue.java
@@ -12,52 +12,52 @@ public class ExQueue {
 
 		Scanner leia = new Scanner(System.in);
 
-		Queue<String> pilha = new LinkedList<String>();
+		Queue<String> cliente = new LinkedList<String>();
 
 		do {
 
 			System.out.println("\n\tBem vindo ao BSF - Banco StackFull :)");
 			System.out.println("*****************************************************");
-			System.out.println("\n1 - Adicionar livro na pilha");
-			System.out.println("\n2 - Listar todos os livros");
-			System.out.println("\n3 - Retirar livro da pilha");
+			System.out.println("\n1 - Adicionar Cliente");
+			System.out.println("\n2 - Listar todos os clientes");
+			System.out.println("\n3 - Retirar cliente da fila");
 			System.out.println("\n0 - Sair");
 			System.out.println("*****************************************************");
-			System.out.println("\nDigite uma ação: ");
+			System.out.println("\nDigite uma opção: ");
 			System.out.println();
 			op = leia.nextInt();
 
 			switch (op) {
 			case 1:
 				leia.nextLine();
-				System.out.println("\nDigite o livro que deseja adicionar: ");
-				String livros = leia.nextLine();
-				pilha.add(livros);
+				System.out.println("\nDigite o cliente que deseja adicionar: ");
+				String nomes = leia.nextLine();
+				cliente.add(nomes);
 				System.out.println("\nNome adicionado!");
 				break;
 
 			case 2:
 				leia.nextLine();
-				System.out.println("\nLista de livros:" +pilha);
+				System.out.println("\nLista de clientes:" + cliente);
 				System.out.println();
 				break;
 
 			case 3:
 				System.out.println();
-				if (pilha.isEmpty() == true) {
+				if (cliente.isEmpty() == true) {
 					System.out.println();
-					System.out.println("\nA pilha está vazia!!!");
+					System.out.println("\nA lista está vazia!!!");
 
 				} else {
-					System.out.println("\nPegar um livro da pilha " +pilha.poll());
+					System.out.println("\nPegar um cliente da lista " + cliente.poll());
 
 				}
 			
 			case 0:
 				System.out.println("\nPrograma finalizado! ");
 				
-				default:
-					System.out.println();
+			default:
+				System.out.println();
 
 			}
 		} while (op != 0);
